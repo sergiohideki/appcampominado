d7278d976169c66c3330dddc2f83697b9e902e6c
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-31 11:52:08 -0300

    classe campo

---
+++ appcampominado/lib/models/campo.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:appcampominado/models/explosao_exception.dart';
+
+class Campo {
+  final int linha;
+  final int coluna;
+  final List<Campo> vizinhos = [];
+
+  bool _aberto = false;
+  bool _marcado = false;
+  bool _minado = false;
+  bool _explodido = false;
+
+  Campo({
+    @required this.linha,
+    @required this.coluna,
+  });
+
+  void adicionarVizinho(Campo vizinho) {
+    final deltaLinha = (linha - vizinho.linha).abs();
+    final deltaColuna = (coluna - vizinho.coluna).abs();
+
+    if (deltaLinha == 0 && deltaColuna == 0) {
+      return;
+    }
+
+    if (deltaLinha <= 1 && deltaColuna <= 1) {
+      vizinhos.add(vizinho);
+    }
+  }
+
+  void abrir() {
+    if (_aberto) {
+      return;
+    }
+
+    if (_minado) {
+      _explodido = true;
+      throw ExplosaoException();
+    }
+
+    if (vizinhancaSegura) {
+      vizinhos.forEach((vizinho) => vizinho.abrir());
+    }
+  }
+
+  void revelarBombas() {
+    if (_minado) {
+      _aberto = true;
+    }
+  }
+
+  void minar() {
+    _minado = true;
+  }
+
+  void alternarMarcacao() {
+    _marcado = !_marcado;
+  }
+
+  void reiniciar() {
+    _aberto = false;
+    _marcado = false;
+    _minado = false;
+    _explodido = false;
+  }
+
+  bool get minado {
+    return _minado;
+  }
+
+  bool get explodido {
+    return _explodido;
+  }
+
+  bool get aberto {
+    return _aberto;
+  }
+
+  bool get marcado {
+    return _marcado;
+  }
+
+  bool get resolvido {
+    bool minadoMarcado = minado && marcado;
+    bool seguroAberto = !minado && aberto;
+    return minadoMarcado || seguroAberto;
+  }
+
+  bool get vizinhancaSegura {
+    return vizinhos.every((vizinho) => !vizinho._minado);
+  }
+
+  int get qtdeMinasVizinhanca{
+    return vizinhos.where((vizinho) => vizinho.minado).length;
+  }
+}
